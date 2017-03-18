09d58640bf580e527394a5919ea377e380bb1178
wangzhengbo <wangzhengbo1984@hotmail.com>
2017-03-18 13:33:23 +0800

    Initial commit.

---
+++ hello.php
@@ -0,0 +1,51 @@
+<html>
+ <head>
+   <title>Hello</title>
+   <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+   <style type="text/css">
+     body {
+       font-family: "Microsoft YaHei",微软雅黑,"Microsoft JhengHei","Hiragino Sans GB",Verdana, Geneva, sans-serif;
+     }
+
+     .wrapper {
+       width: 900px;
+       margin: 0 auto;
+     }
+
+     p {
+       margin-top: 20px;
+     }
+
+     a:link {
+       text-decoration: none;
+       color: #2d59a2;
+     }
+
+     a:visited {
+       text-decoration: none;
+       color: #2d59a2;
+     }
+
+     a:hover {
+       text-decoration: none;
+       color: #203f71;
+     }
+
+     a:active {
+       text-decoration: none;
+       color: #203f71;
+     }
+   </style>
+ </head>
+ <body>
+  <div class="wrapper">
+  <h1> Hello PHP!</h1>
+   <p>这是 <a href="https://www.goodrain.com" target="_blank">好雨</a> 云平台 <a href="http://code.goodrain.com/demo/php-hello/tree/master" target="_blank">PHP</a> 代码演示效果页面。</p>
+   <p>
+     本页面是一个静态页面，动态页面示例：<a href='phpinfo.php'  style="color: green; text-decoration: underline;">查看phpinfo信息</a>
+   </p>
+   <p>恭喜! PHP代码已成功构建并运行! </p>
+
+  </div>
+ </body>
+</html>
